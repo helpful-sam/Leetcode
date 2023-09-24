f279eec443114a06f0b62ee3e6ef29d565bdd43a
Sam <harrow48625@gmail.com>
2023-09-24 12:31:56 +0900

    pseudo code added to 1048

--- 1048. Longest String Chain/1048 attempt 1.py
+++ 1048. Longest String Chain/1048 attempt 1.py
@@ -4,3 +4,14 @@ class Solution(object):
         :type words: List[str]
         :rtype: int
         """
+
+        # 1. Order strings from shortest to longest
+        # 2. Create an array of ordered pairs (tuples) with the format of (string length, highest index of this-length string)
+        # 3. For each strings from 0 to "shortest_upper_index", calculate chain length
+            # a. split current_string and next_string into arrays
+            # b. compare current_string[0] to next_string[0] and next_string[1]
+                # bb. if no match, move next_string pointer by 1
+                # bb. if match, continue
+            # c. compare current_string[1] to next_string[1] and next_string[2] or next_string[2] and next_string[3]
+                # note. use upper and lower letter pointers to shift reading frame by 1 or 2 depending on where the match was in step b
+        # 4. Return longest chain length as integer 
