45caeaaf799a0fe064a14a2823ba1cf0ca3946b4
Sam <harrow48625@gmail.com>
2023-09-25 19:38:25 +0900

    solved 1048 with the help of ChatGPT

--- 1048. Longest String Chain/1048 attempt 1.py
+++ 1048. Longest String Chain/1048 attempt 1.py
@@ -5,13 +5,55 @@ class Solution(object):
         :rtype: int
         """
 
+        ### PSEUDO CODE ###
+        
         # 1. Order strings from shortest to longest
         # 2. Create an array of ordered pairs (tuples) with the format of (string length, highest index of this-length string)
         # 3. For each strings from 0 to "shortest_upper_index", calculate chain length
-            # a. split current_string and next_string into arrays
-            # b. compare current_string[0] to next_string[0] and next_string[1]
-                # bb. if no match, move next_string pointer by 1
-                # bb. if match, continue
-            # c. compare current_string[1] to next_string[1] and next_string[2] or next_string[2] and next_string[3]
-                # note. use upper and lower letter pointers to shift reading frame by 1 or 2 depending on where the match was in step b
-        # 4. Return longest chain length as integer 
+        # 4. Return longest chain
+
+
+        ### ACTUAL CODE ###
+
+        # 1. sort #
+
+        sorted_words = sorted(words, key=len)
+
+        # 2. indexing #
+
+        categories = {key: 0 for key in range(1,17)}
+
+        for index in range(len(words)):
+            word_length = len(words[index])
+            
+            if word_length in categories:
+                categories[word_length] += 1
+
+        # 3. chain length calculation #
+
+        # dictionary to store length information
+        chain_lengths = {}
+
+        # calculation function
+        def calculate_chain_length(word):
+            if word in chain_lengths:
+                return chain_lengths[word]
+            
+            max_length = 1  # Minimum chain length is 1 (the word itself)
+            
+            # Try removing each character from the word
+            for i in range(len(word)):
+                new_word = word[:i] + word[i+1:]
+                if new_word in words:
+                    max_length = max(max_length, 1 + calculate_chain_length(new_word))
+            
+            chain_lengths[word] = max_length
+            return max_length
+        
+        # recursive logic #
+        max_chain_length = 0
+        for word in sorted_words:
+            max_chain_length = max(max_chain_length, calculate_chain_length(word))
+
+        # 4. return longest chain length #
+        return max_chain_length
